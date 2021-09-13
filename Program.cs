b8a04638d5c095cca2c2781ab4d8615f063e6f95
VikasDhanwate <89641546+VikasDhanwate@users.noreply.github.com>
2021-09-13 16:38:06 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EvenOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int num,i;
+
+            for(num=2;num<=10;num++)
+                for(i=1;i<=10;i++)
+                {
+                    Console.WriteLine(num + " * " +i + " = " +(num * i));
+                }
+            
+            if (num % 2 ==0)
+            {
+                Console.WriteLine(num + " Even");
+            }
+            else
+            {
+                Console.WriteLine(num + " Odd");
+            }
+        }
+    }
+}
